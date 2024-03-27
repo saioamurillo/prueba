4d888ef3debd256fc48d6f87c003ac713e761fc1
saioamurillo <151669972+saioamurillo@users.noreply.github.com>
2024-03-27 14:46:50 +0100

    Reconocimiento de audio y dos codigos

--- script.js
+++ script.js
@@ -1,114 +1,204 @@
 var canvas = document.getElementById('gradient');
 var ctx = canvas.getContext('2d');
-
-// DEGRADADO
 canvas.width = 256;
 canvas.height = 256;
-
 var gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
 gradient.addColorStop(0, 'white'); 
 gradient.addColorStop(1, 'green'); 
-
 ctx.fillStyle = gradient;
 ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-//CLIC Y ANIMACIONES
 
 AFRAME.registerComponent('details-listener', {
     init: function () {
         var el = this.el; 
-        var buttonClicked = false;
-        var detailsButton = document.querySelector('#details-button');
-        var planoAzul = document.querySelector('#details-box')
+        console.log("init");
 
-
-        el.addEventListener('click', function (evt) {
-            if (!buttonClicked) { 
-                console.log("Clic detectado en Ver Detalles."); 
-                buttonClicked = true; 
-
-                detailsButton.setAttribute('value', 'Ocultar detalles'); // Cambiar el texto a "Ocultar detalles"
-               planoAzul.setAttribute("color", "gray")
-
-                // Agregar el círculo y las esferas
-                var newCircle = document.createElement('a-circle');
-                newCircle.setAttribute('id', 'new-circle'); // Agregar un ID para identificarlo fácilmente
-                newCircle.setAttribute('radius', '0.25');
-                newCircle.setAttribute('color', 'white');
-                newCircle.setAttribute('position', '0 -0.7 0'); 
-                newCircle.setAttribute('rotation', '0 45 0');
-                newCircle.setAttribute('animation', 'property: rotation; to: 0 360 0; dur: 1000; easing: linear');
-                newCircle.setAttribute('material', 'shader: flat; src: #gradient'); 
-
-                var newText = document.createElement('a-text');
-                newText.setAttribute('value', 'Detalles');
-                newText.setAttribute('align', 'center');
-                newText.setAttribute('position', '0 0 0.05');
-                newText.setAttribute('color', 'black');
-                newText.setAttribute('scale', '0.3 0.3 0.3'); 
-                newCircle.appendChild(newText);
-
-                var radius = 0.35; 
-                var numSpheres = 8; 
-                var angleIncrement = (2 * Math.PI) / numSpheres; 
-                for (var i = 0; i < numSpheres; i++) {
-                    var angle = i * angleIncrement; 
-                    var x = radius * Math.cos(angle); 
-                    var y = radius * Math.sin(angle); 
-
-                    var sphere = document.createElement('a-sphere');
-                    sphere.setAttribute('radius', '0.05');
-                    sphere.setAttribute('color', 'green');
-                    sphere.setAttribute('position', x + ' ' + y + ' 0'); 
-                    sphere.setAttribute('animation', 'property: rotation; to: 0 360 0; dur: 2000; easing: linear; loop: true'); 
-                    newCircle.appendChild(sphere);
-                }
-
-                el.parentNode.appendChild(newCircle); 
-
-            } else {
-                console.log("Clic detectado en Ocultar Detalles.");
-                buttonClicked = false;
-
-                detailsButton.setAttribute('value', 'Ver detalles'); // Cambiar el texto a "Ver detalles"
-                planoAzul.setAttribute("color", "green")
+navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(function (stream) {
+        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+        recognition.lang = "es-ES";
+        const inputNode = audioContext.createMediaStreamSource(stream);
+        const analyser = audioContext.createAnalyser();
+        analyser.fftSize = 2048;
+        const bufferLength = analyser.frequencyBinCount;
+        const dataArray = new Uint8Array(bufferLength);
+        inputNode.connect(analyser);
+        recognition.onstart = function () {
+            console.log("El reconocimiento de voz ha comenzado");
+        };
+        recognition.onend = function () {
+            console.log("El reconocimiento de voz ha terminado. Reiniciando...");
+            recognition.start();
+        };
+        recognition.onresult = function (event) {
+            const transcript = event.results[0][0].transcript;
+            console.log("Texto reconocido:", transcript);
+            const textoNormalizado = transcript
+                .toLowerCase()
+                .replace(/[^\w\s]/gi, "");
+            if (textoNormalizado.includes("ver detalles covid")) {
+                console.log("Te he escuchado");
                 
-                // Eliminar el círculo y las esferas si existen
-                var newCircle = document.querySelector('#new-circle');
-                if (newCircle) {
-                    newCircle.parentNode.removeChild(newCircle);
-                }
-            }
-        });
-    }
-});
+                const token =
+                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6WyJhZG1pbiIsIkN1c3RvbU9iamVjdENhbkJlQWRkZWRIZXJlIl0sIm5iZiI6MTcxMTM1OTk4NiwiZXhwIjoxNzExOTY0Nzg2LCJpYXQiOjE3MTEzNTk5ODZ9.7DL7B5eudtcZANhgXVexHDOqE4yzj1ijozpbkVXfyqc";
+                const headers = {
+                    Authorization: `Bearer ${token}`,
+                };
+                fetch("https://207.180.229.60:9443/v1/api/CAJAS/7063", {
+                    method: "GET",
+                    headers: headers,
+                })
+                    .then((res) => res.json())
+                    .then((res) => {
+                        console.log(res);
+                        visualizacion(res);                      
+                    })
+                    .catch((error) => console.error(error));
+                    
+                } else if (textoNormalizado.includes("ver detalles adn")) {
+                    console.log("Te he escuchado");
+                
+                    const token =
+                        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6WyJhZG1pbiIsIkN1c3RvbU9iamVjdENhbkJlQWRkZWRIZXJlIl0sIm5iZiI6MTcxMTM1OTk4NiwiZXhwIjoxNzExOTY0Nzg2LCJpYXQiOjE3MTEzNTk5ODZ9.7DL7B5eudtcZANhgXVexHDOqE4yzj1ijozpbkVXfyqc";
+                    const headers = {
+                        Authorization: `Bearer ${token}`,
+                    };
+                    fetch("https://207.180.229.60:9443/v1/api/CAJAS/7063", {
+                        method: "GET",
+                        headers: headers,
+                    })
+                        .then((res) => res.json())
+                        .then((res) => {
+                            console.log(res);
+                            visualizacionADN('adn-marker', res);                      
+                        })
+                        .catch((error) => console.error(error));
+                
+                
+                }else if (textoNormalizado.includes("ocultar detalles covid")) {
+                    console.log("Ocultar detalles Covid");
+                
+                    var circleToRemove = document.getElementById('new-circle-covid');
+                    if (circleToRemove) {
+                        circleToRemove.parentNode.removeChild(circleToRemove);
+                    }
+                }else if (textoNormalizado.includes("ocultar detalles adn")) {
+                    console.log("Ocultar detalles ADN");
+                
+                    var circleToRemove = document.getElementById('new-circle-adn');
+                    if (circleToRemove) {
+                        circleToRemove.parentNode.removeChild(circleToRemove);
+                    }
+                    
+        }
+
+        };
+
+        recognition.onerror = function (event) {
+            console.error("Error de reconocimiento de voz:", event.error);
+        };
+
+        function visualize() {
+            analyser.getByteTimeDomainData(dataArray);
+            requestAnimationFrame(visualize);
+        }
+        visualize();
+        recognition.start();
 
 
-// Configurar el raycaster
-document.addEventListener('DOMContentLoaded', function () {
-    var sceneEl = document.querySelector('a-scene');
-    sceneEl.addEventListener('loaded', function () {
-        var raycasterEl = document.querySelector('[raycaster]');
-        raycasterEl.setAttribute('raycaster', 'objects: .clickable');
+    })
+    
+    .catch(function (err) {
+        console.error("Error al obtener el flujo de audio:", err);
     });
-});
 
-// Escucha cambios en la posición del objeto detectado (planoAzul)
-var planoAzul = document.querySelector('#details-box');
-planoAzul.addEventListener('componentchanged', function (evt) {
-    if (evt.detail.name === 'position') {
-        // Cuando la posición de planoAzul cambie, actualiza la posición del botón "Ver detalles"
-        var newPosition = planoAzul.getAttribute('position');
-        detailsButton.setAttribute('position', newPosition);
+    function visualizacion(objeto) {
+
+        var newCircle = document.createElement('a-circle');
+        newCircle.setAttribute('id', 'new-circle-covid'); 
+        newCircle.setAttribute('radius', '0.25');
+        newCircle.setAttribute('color', 'white');
+        newCircle.setAttribute('position', '0 -1.2 0'); 
+        newCircle.setAttribute('rotation', '0 45 0');
+        newCircle.setAttribute('animation', 'property: rotation; to: 0 360 0; dur: 1000; easing: linear');
+        newCircle.setAttribute('material', 'shader: flat; src: #gradient'); 
+
+        var newText = document.createElement('a-text');
+        newText.setAttribute('value',`Code:${objeto.code}\n${objeto.message}\n${objeto.document.CAJA_ID}`); 
+        newText.setAttribute('align', 'center');
+        newText.setAttribute('position', '0 0 0.05');
+        newText.setAttribute('color', 'black');
+        newText.setAttribute('scale', '0.3 0.3 0.3'); 
+        newCircle.appendChild(newText);
+
+        var radius = 0.35; 
+        var numSpheres = 8; 
+        var angleIncrement = (2 * Math.PI) / numSpheres; 
+        for (var i = 0; i < numSpheres; i++) {
+            var angle = i * angleIncrement; 
+            var x = radius * Math.cos(angle); 
+            var y = radius * Math.sin(angle); 
+
+            var sphere = document.createElement('a-sphere');
+            sphere.setAttribute('radius', '0.05');
+            sphere.setAttribute('color', 'green');
+            sphere.setAttribute('position', x + ' ' + y + ' 0'); 
+            sphere.setAttribute('animation', 'property: rotation; to: 0 360 0; dur: 2000; easing: linear; loop: true'); 
+            newCircle.appendChild(sphere);
+        }
+    
+        el.parentNode.appendChild(newCircle); 
     }
-});
+    function visualizacionADN(markerId, objeto) {
+        const marker = document.getElementById(markerId);
+        if (!marker) return;
 
-// Agregar el componente 'details-listener'
+        var newCircle = document.createElement('a-circle');
+        newCircle.setAttribute('id', 'new-circle-adn'); 
+        newCircle.setAttribute('radius', '0.25');
+        newCircle.setAttribute('color', 'violet');
+        newCircle.setAttribute('position', '0 0 1'); 
+        newCircle.setAttribute('rotation', '-90 0 0');
+        newCircle.setAttribute('animation', 'property: rotation; to:  270 0 0; dur: 1000; easing: linear');
+
+        var newText = document.createElement('a-text');
+        newText.setAttribute('value',`Code:${objeto.code}\n${objeto.message}\n${objeto.document.CAJA_ID}`); 
+        newText.setAttribute('align', 'center');
+        newText.setAttribute('position', '0 0 0.05');
+        newText.setAttribute('color', 'black');
+        newText.setAttribute('scale', '0.3 0.3 0.3'); 
+        newCircle.appendChild(newText);
+
+        const radius = 0.35;
+        const numSpheres = 8;
+        const angleIncrement = (2 * Math.PI) / numSpheres;
+        for (let i = 0; i < numSpheres; i++) {
+            const angle = i * angleIncrement;
+            const x = radius * Math.cos(angle);
+            const y = radius * Math.sin(angle);
+            const sphere = document.createElement('a-sphere');
+            sphere.setAttribute('radius', '0.05');
+            sphere.setAttribute('color', 'purple');
+            sphere.setAttribute('position', `${x} ${y} 0`);
+            sphere.setAttribute('animation', 'property: rotation; to: 0 360 0; dur: 2000; easing: linear; loop: true');
+            newCircle.appendChild(sphere);
+        }
+        marker.appendChild(newCircle);
+    }
+}
+    
+})
+
 document.addEventListener('DOMContentLoaded', function () {
     var detailsBox = document.querySelector('#details-box');
     if (detailsBox) {
+        console.log("existe")
         detailsBox.setAttribute('details-listener', '');
     }
 });
+    
 
+    
+    
 
