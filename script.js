d890970a4def4b29ad9b612ca391ecfbc7f0839d
saioamurillo <151669972+saioamurillo@users.noreply.github.com>
2024-04-03 10:29:07 +0200

    Update script.js Audio+aframe

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+var canvas = document.getElementById('gradient');
+var ctx = canvas.getContext('2d');
+canvas.width = 256;
+canvas.height = 256;
+var gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+gradient.addColorStop(0, 'white'); 
+gradient.addColorStop(1, 'green'); 
+ctx.fillStyle = gradient;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+AFRAME.registerComponent('details-listener', {
+    init: function () {
+        var el = this.el; 
+        console.log("init");
+
+        const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
 recognition.lang = "es-ES";
 recognition.onstart = function () {
     console.log("El reconocimiento de voz ha comenzado");
@@ -71,6 +86,95 @@ recognition.onerror = function (event) {
     console.error("Error de reconocimiento de voz:", event.error);
 };
 recognition.start();
+
+    function visualizacion(objeto) {
+
+
+        
+        var newCircle = document.createElement('a-circle');
+        newCircle.setAttribute('id', 'new-circle-covid'); 
+        newCircle.setAttribute('radius', '0.25');
+        newCircle.setAttribute('color', 'white');
+        newCircle.setAttribute('position', '0 -1.2 0'); 
+        newCircle.setAttribute('rotation', '0 45 0');
+        newCircle.setAttribute('animation', 'property: rotation; to: 0 360 0; dur: 1000; easing: linear');
+        newCircle.setAttribute('material', 'shader: flat; src: #gradient'); 
+
+        var newText = document.createElement('a-text');
+        newText.setAttribute('value',`Code:${objeto.code}\n${objeto.message}\n${objeto.document.CAJA_ID}`); 
+        newText.setAttribute('align', 'center');
+        newText.setAttribute('position', '0 0 0.05');
+        newText.setAttribute('color', 'black');
+        newText.setAttribute('scale', '0.3 0.3 0.3'); 
+        newCircle.appendChild(newText);
+
+        var radius = 0.35; 
+        var numSpheres = 8; 
+        var angleIncrement = (2 * Math.PI) / numSpheres; 
+        for (var i = 0; i < numSpheres; i++) {
+            var angle = i * angleIncrement; 
+            var x = radius * Math.cos(angle); 
+            var y = radius * Math.sin(angle); 
+
+            var sphere = document.createElement('a-sphere');
+            sphere.setAttribute('radius', '0.05');
+            sphere.setAttribute('color', 'green');
+            sphere.setAttribute('position', x + ' ' + y + ' 0'); 
+            sphere.setAttribute('animation', 'property: rotation; to: 0 360 0; dur: 2000; easing: linear; loop: true'); 
+            newCircle.appendChild(sphere);
+        }
+    
+        el.parentNode.appendChild(newCircle); 
+    }
+    function visualizacionADN(markerId, objeto) {
+        const marker = document.getElementById(markerId);
+        if (!marker) return;
+
+        var newCircle = document.createElement('a-circle');
+        newCircle.setAttribute('id', 'new-circle-adn'); 
+        newCircle.setAttribute('radius', '0.25');
+        newCircle.setAttribute('color', 'violet');
+        newCircle.setAttribute('position', '0 0 1'); 
+        newCircle.setAttribute('rotation', '-90 0 0');
+        newCircle.setAttribute('animation', 'property: rotation; to:  270 0 0; dur: 1000; easing: linear');
+
+        var newText = document.createElement('a-text');
+        newText.setAttribute('value',`Code:${objeto.code}\n${objeto.message}\n${objeto.document.CAJA_ID}`); 
+        newText.setAttribute('align', 'center');
+        newText.setAttribute('position', '0 0 0.05');
+        newText.setAttribute('color', 'black');
+        newText.setAttribute('scale', '0.3 0.3 0.3'); 
+        newCircle.appendChild(newText);
+
+        const radius = 0.35;
+        const numSpheres = 8;
+        const angleIncrement = (2 * Math.PI) / numSpheres;
+        for (let i = 0; i < numSpheres; i++) {
+            const angle = i * angleIncrement;
+            const x = radius * Math.cos(angle);
+            const y = radius * Math.sin(angle);
+            const sphere = document.createElement('a-sphere');
+            sphere.setAttribute('radius', '0.05');
+            sphere.setAttribute('color', 'purple');
+            sphere.setAttribute('position', `${x} ${y} 0`);
+            sphere.setAttribute('animation', 'property: rotation; to: 0 360 0; dur: 2000; easing: linear; loop: true');
+            newCircle.appendChild(sphere);
+        }
+        marker.appendChild(newCircle);
+    }
+}
+    
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    var detailsBox = document.querySelector('#details-box');
+    if (detailsBox) {
+        console.log("existe")
+        detailsBox.setAttribute('details-listener', '');
+    }
+});
+    
+
     
 
     
